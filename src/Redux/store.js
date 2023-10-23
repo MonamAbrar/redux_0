d7fcb01e316a45fa9e44e01080133fd95a1945fa
Monam Abrar <mabrar@soloinsight.com>
2023-10-23 15:31:32 +0500

    0.0.4

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,8 +3,11 @@
 import { createStore } from "redux";
 import mainReducer from './Reducers/mainReducer';
 
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const store = createStore(mainReducer);
+
+// const store = createStore(mainReducer)
+const store = createStore(mainReducer, composeWithDevTools());
 
 
 // console.log(store.getState());
